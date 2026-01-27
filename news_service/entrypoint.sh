d17c91136dc4416e53dcd06bc9f6a5db5a77acb0
sofii4 <sofialopesdasilva00@gmail.com>
2026-01-27 17:09:33 +0000

    Fix: Traefik routes corrections and Flask prefixes for News and Users

--- news_service/entrypoint.sh
+++ news_service/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# 1. Dá ao 'appuser' a propriedade da pasta de uploads.
-#    Isso conserta o volume montado no 'run time'.
+# 1. Ajusta permissões do volume em tempo de execução
 chown -R appuser:appuser /app/app/static/uploads
 
-# 2.  Garante que o 'appuser' é dono de tudo.
-chown -R appuser:appuser /app
+# 2. (Opcional) Aguardar o Banco de Dados
+# Isso evita que o Flask tente conectar antes do MariaDB estar pronto
+echo "Aguardando o banco de dados..."
+sleep 5 
 
-# 3. Entrega o controle para o 'CMD' (o gunicorn),
-#    mas roda ele como o 'appuser' (drop de privilégios).
+# 3. Executa o CMD do Dockerfile como appuser
+echo "Iniciando Gunicorn como appuser..."
 exec gosu appuser "$@"
